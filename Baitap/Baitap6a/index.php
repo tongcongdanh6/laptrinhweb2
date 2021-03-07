245dd1ca401361734ac2f8ce3ea5948e6377af00
tongcongdanh6 <tong.congdanh@gmail.com>
2021-03-07 23:19:00 +0700

    Add files via upload

---
+++ Baitap/Baitap6a/index.php
@@ -0,0 +1,20 @@
+﻿<?php
+if(!isset($_GET["q"])) {
+  $title = "Thông tin sinh viên";
+  $content = "thongtinsv";
+  include("views/layout.php");
+}
+else {
+  switch($_GET["q"]) {
+    case "imagesharing":
+      $title = "Chia sẻ hình ảnh";
+      $content = "imagesharing";
+      include("views/layout.php");
+      break;
+    default:
+      $title = "Thông tin sinh viên";
+      $content = "thongtinsv";
+      include("views/layout.php");
+      break;
+  }
+}
---
+++ Baitap/Baitap6a/views/pages/thongtinsv.php
@@ -0,0 +1,4 @@
+<div class="student-info">
+    <h2>Tống Công Danh</h2>
+    <h3>MSSV: 1988216</h3>
+</div>
